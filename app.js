3a2b6ec0518fca7ac8d1197c7f4e72941aa9a452
BigBlueHat <byoung@bigbluehat.com>
2014-05-07 13:44:01 -0400

    added other (ajax loadable) schemas

--- app.js
+++ app.js
@@ -31,6 +31,35 @@ Vue.component('json-textarea', {
   }
 });
 
+Vue.component('schema-links', {
+  data: {
+    links: {
+      "Example Schema": "schemas/example.schema.json",
+      "Address": "schemas/address.schema.json",
+      "Calendar": "schemas/calendar.schema.json",
+      "Card": "schemas/card.schema.json",
+      "Geo": "schemas/geo.schema.json",
+      "Hyper-schema": "schemas/hyper-schema.schema.json",
+      "Interfaces": "schemas/interfaces.schema.json",
+      "JSON-Ref": "schemas/json-ref.schema.json",
+      "Schema": "schemas/schema.schema.json"
+    }
+  },
+  methods: {
+    setHref: function(e) {
+      e.preventDefault();
+
+      var xhr = new XMLHttpRequest(),
+          self = this;
+      xhr.open('GET', e.target.getAttribute('href'));
+      xhr.onload = function () {
+        self.$parent.schema = JSON.parse(xhr.responseText);
+      }
+      xhr.send()
+    }
+  }
+});
+
 var app = new Vue({
   el: '#editor',
   data: {
